0b910e0f40512d71aceff18271696064506cedb1
Al Nguyen <al.nguyen@gmail.com>
2024-01-09 12:08:07 -0500

    Use a channel to broadcast messages to everyone

--- index.ts
+++ index.ts
@@ -1,5 +1,14 @@
+// References
+// - https://github.com/facundofarias/awesome-websockets
+
+const initialFEN = 'aqabvrvnbrbnbbbqbkbbbnbrynyrsbsq/aranvpvpbpbpbpbpbpbpbpbpypypsnsr/nbnp12opob/nqnp12opoq/crcp12rprr/cncp12rprn/gbgp12pppb/gqgp12pppq/yqyp12vpvq/ybyp12vpvb/onop12npnn/orop12npnr/rqrp12cpcq/rbrp12cpcb/srsnppppwpwpwpwpwpwpwpwpgpgpanar/sqsbprpnwrwnwbwqwkwbwnwrgngrabaq';
+const CHANNEL = "game-0";
+let fenCache;
+
 const server = Bun.serve<{ authToken: string }>({
   fetch(req, server) {
+    // TODO: Use Content Security Policy directives to secure connection
+    // https://content-security-policy.com/
     const success = server.upgrade(req);
     if (success) {
       return undefined;
@@ -8,9 +17,20 @@ const server = Bun.serve<{ authToken: string }>({
     return new Response("Hello world!");
   },
   websocket: {
-    async message(ws, message) {
+    open(ws) {
+      ws.subscribe(CHANNEL);
+      ws.send(fenCache || initialFEN);
+      console.log(`fenCache: ${fenCache}`);
+    },
+    message(ws, message) {
       console.log(`Received ${message}`);
-      ws.send(`Ack: ${message}`);
+      //ws.send(`Ack: ${message}`);
+      fenCache = message;
+      console.log(`fenCache: ${fenCache}`);
+      ws.publish(CHANNEL, message);
+    },
+    close(ws) {
+      ws.unsubscribe(CHANNEL);
     },
   },
 });
