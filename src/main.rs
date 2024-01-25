8ebab498dc006df030c7397040c5829734004918
Al Nguyen <al.nguyen@gmail.com>
2024-01-25 10:14:04 -0500

    Add tracing

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,16 @@ use futures::{sink::SinkExt, stream::StreamExt};
 use std::{
     sync::{Arc, Mutex},
 };
+use tower_http::trace::TraceLayer;
 use tokio::sync::broadcast;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 const SOCKET_ADDRESS: &'static str = "127.0.0.1:3000";
 const INITIAL_FEN: &'static str = "aqabvrvnbrbnbbbqbkbbbnbrynyrsbsq/aranvpvpbpbpbpbpbpbpbpbpypypsnsr/nbnp12opob/nqnp12opoq/crcp12rprr/cncp12rprn/gbgp12pppb/gqgp12pppq/yqyp12vpvq/ybyp12vpvb/onop12npnn/orop12npnr/rqrp12cpcq/rbrp12cpcb/srsnppppwpwpwpwpwpwpwpwpgpgpanar/sqsbprpnwrwnwbwqwkwbwnwrgngrabaq";
 
 type SharedState = Arc<AppState>;
 
+#[derive(Debug)]
 struct AppState {
     fen: Mutex<String>,
     tx: broadcast::Sender<String>,
@@ -25,16 +28,26 @@ struct AppState {
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                "sochess_be=debug,tower_http=debug,axum::rejection=trace".into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     let (tx, _) = broadcast::channel(100);
     let app_state = Arc::new(AppState { fen: Mutex::new(INITIAL_FEN.to_string()), tx });
 
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
         .route("/ws", get(ws_upgrade_handler))
+        .layer(TraceLayer::new_for_http())
         .with_state(app_state);
 
     let listener = tokio::net::TcpListener::bind(SOCKET_ADDRESS).await.unwrap();
-    println!("> Listening on http://{}", listener.local_addr().unwrap());
+    tracing::debug!("> Listening on http://{}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
 
@@ -43,6 +56,10 @@ async fn ws_upgrade_handler(ws: WebSocketUpgrade, State(state): State<SharedStat
 }
 
 async fn handle_socket(socket: WebSocket, state: SharedState) {
+    let span = tracing::info_span!("handle_socket");
+    let _enter = span.enter();
+    tracing::info!("connection opened");
+
     let (mut sender, mut receiver) = socket.split();
 
     {
@@ -63,7 +80,7 @@ async fn handle_socket(socket: WebSocket, state: SharedState) {
         while let Some(Ok(Message::Text(msg))) = receiver.next().await {
             // Update game state
             *cloned_state.fen.lock().unwrap() = msg.clone();
-            println!("msg={:?}", msg);
+            tracing::info!("received msg={}", msg);
 
             let _ = tx.send(msg);
         }
@@ -85,4 +102,6 @@ async fn handle_socket(socket: WebSocket, state: SharedState) {
         _ = (&mut send_task) => recv_task.abort(),
         _ = (&mut recv_task) => send_task.abort(),
     };
+
+    tracing::info!("connection closed");
 }
