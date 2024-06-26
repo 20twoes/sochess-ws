e33557605a098a47fcc26f10d84511629cd4ea8b
Al Nguyen <al.nguyen@gmail.com>
2024-06-26 12:03:20 -0400

    Add ability to make first move of game

--- src/game_handler.rs
+++ src/game_handler.rs
@@ -35,10 +35,12 @@ impl GameHandler {
                 user: user,
                 db: db_handle,
             },
-            //GameState::Accepted => GameHandler::<Accepted + 'static> {
-            //    game: game.clone(),
-            //    marker: std::marker::PhantomData,
-            //},
+            GameState::Accepted => Self {
+                state: Some(Box::new(Accepted {})),
+                game: game,
+                user: user,
+                db: db_handle,
+            },
             _ => todo!(),
         }
     }
@@ -49,17 +51,19 @@ impl GameHandler {
     }
 
     pub async fn process(&mut self, json: serde_json::Value) -> Result<(), &'static str> {
-        println!("process 0:  {:?}", json);
         match json["t"].as_str() {
             Some("join") => {
-                println!("process 1");
                 if let Some(s) = self.state.take() {
-                    println!("process 2");
                     self.state = Some(
-                        s.join_game(&self.db, &mut self.game, &self.user)
-                            .await
-                            .unwrap(),
+                        s.join_game(self).await.unwrap(),
                     );
+                }
+                Ok(())
+            }
+            Some("move") => {
+                if let Some(s) = self.state.take() {
+                    let new_move = json["d"].as_str().unwrap();
+                    self.state = Some(s.add_move(self, new_move.to_string()).await.unwrap());
                 }
                 Ok(())
             }
@@ -70,12 +74,18 @@ impl GameHandler {
 
 #[async_trait]
 trait HandlerState {
+    #[allow(unused_variables)]
     async fn join_game(
         &self,
-        db_handle: &Database,
-        game: &mut Game,
-        user: &User,
+        handler: &mut GameHandler,
     ) -> Result<Box<Accepted>, GameHandlerError> {
+        Err(GameHandlerError {
+            message: "Forbidden game action".to_string(),
+        })
+    }
+
+    #[allow(unused_variables)]
+    async fn add_move(&self, handler: &mut GameHandler, new_move: String) -> Result<Box<FirstMove>, GameHandlerError> {
         Err(GameHandlerError {
             message: "Forbidden game action".to_string(),
         })
@@ -84,20 +94,28 @@ trait HandlerState {
 
 struct Created {}
 struct Accepted {}
+struct FirstMove {}
 
 #[async_trait]
 impl HandlerState for Created {
     async fn join_game(
         &self,
-        db_handle: &Database,
-        game: &mut Game,
-        user: &User,
+        handler: &mut GameHandler,
     ) -> Result<Box<Accepted>, GameHandlerError> {
-        println!("running join_game");
-        game.set_player_joined(user);
-        db::update_player(db_handle, game, &user.name).await;
+        handler.game.set_player_joined(&handler.user);
+        db::update_player(&handler.db, &handler.game, &handler.user.name).await;
         Ok(Box::new(Accepted {}))
     }
 }
 
-impl HandlerState for Accepted {}
+#[async_trait]
+impl HandlerState for Accepted {
+    async fn add_move(&self, handler: &mut GameHandler, new_move: String) -> Result<Box<FirstMove>, GameHandlerError> {
+        println!("running join_game");
+        handler.game.add_move(new_move.clone());
+        db::save_game_move(&handler.db, &handler.game).await;
+        Ok(Box::new(FirstMove {}))
+    }
+}
+
+impl HandlerState for FirstMove {}
