0710cecc53d89c81fb4d61518c1c5799f4383d27
Al Nguyen <al.nguyen@gmail.com>
2024-08-29 17:13:11 -0400

    Shorten the game id

--- src/game.rs
+++ src/game.rs
@@ -39,8 +39,16 @@ pub struct Game {
 
 impl Game {
     pub fn new() -> Self {
+        let id_len = 12;
+        let valid_chars: [char; 62] = [
+            '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+            'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+            'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+            'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+        ];
+
         Self {
-            pid: nanoid!(),
+            pid: nanoid!(id_len, &valid_chars),
             created: Utc::now(),
             updated: Utc::now(),
             moves: vec![Default::default()],
